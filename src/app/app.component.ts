993a0a95c4711d6250dfbc0d49dfb9e1e8c73f02
fboeller <fabianboeller@googlemail.com>
2020-05-08 23:24:30 +0200

    Add an incrementing counter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable, interval } from 'rxjs';
+import { mapTo, scan } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.styl']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'upgrade-game';
+
+  counterValue$: Observable<number>;
+
+  ngOnInit() {
+    this.counterValue$ = interval(1000)
+      .pipe(
+        mapTo(1),
+        scan((acc, inc) => acc + inc)
+      )
+  }
+
 }
