d2cc6e81de42bc9f6da130fe729a29ddb5d457fa
fboeller <fabianboeller@googlemail.com>
2020-05-09 21:17:48 +0200

    Increasing update costs

--- src/app/personal-panel/personal-panel.component.ts
+++ src/app/personal-panel/personal-panel.component.ts
@@ -1,6 +1,15 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
-import { Observable, Subject } from 'rxjs';
-import { mapTo, scan, startWith, map, filter, first } from 'rxjs/operators';
+import { Observable, Subject, range, zip, generate } from 'rxjs';
+import {
+  scan,
+  startWith,
+  map,
+  filter,
+  first,
+  withLatestFrom,
+  skip,
+  tap,
+} from 'rxjs/operators';
 import { Upgrade } from '../upgrade';
 import { trigger, transition, animate, style } from '@angular/animations';
 
@@ -29,24 +38,44 @@ export class PersonalPanelComponent implements OnInit {
 
   salary$: Observable<number>;
   upgradePurchase$: Observable<Upgrade>;
+  nextPurchase$: Observable<Upgrade>;
   salaryUpgradePossible$: Observable<boolean>;
   panelVisible$: Observable<boolean>;
-  salaryUpgrade: Upgrade = {
-    property: 'Salary',
-    cost: 3,
-    update: (x: number) => x + 1,
-  };
+  salaryUpgrade$: Observable<Upgrade>;
 
   ngOnInit(): void {
-    this.upgradePurchase$ = this.increaseSalaryButtonClicked$.pipe(
-      mapTo(this.salaryUpgrade)
+    this.salaryUpgrade$ = range(3, 1000).pipe(
+      map((cost) => ({
+        property: 'Salary',
+        cost,
+        update: (x: number) => x + 1,
+      }))
+    );
+    this.upgradePurchase$ = zip(
+      this.increaseSalaryButtonClicked$,
+      this.salaryUpgrade$,
+      (_, upgrade) => upgrade
+    );
+    this.nextPurchase$ = zip(
+      this.increaseSalaryButtonClicked$,
+      this.salaryUpgrade$.pipe(skip(1)),
+      (_, nextUpgrade) => nextUpgrade
+    ).pipe(
+      startWith({
+        property: 'Salary',
+        cost: 3,
+        update: (x: number) => x + 1,
+      })
     );
     this.salary$ = this.upgradePurchase$.pipe(
       scan((salary, upgrade) => upgrade.update(salary), 1),
       startWith(1)
     );
     this.salaryUpgradePossible$ = this.funds$.pipe(
-      map((value) => value >= this.salaryUpgrade.cost)
+      withLatestFrom(
+        this.nextPurchase$,
+        (value, nextPurchase) => value >= nextPurchase.cost
+      )
     );
     this.panelVisible$ = this.salaryUpgradePossible$.pipe(
       filter((possible) => possible),
