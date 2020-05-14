e0997d22283b4ab04bd64d8bb0f88d788c21d431
fboeller <fabianboeller@googlemail.com>
2020-05-14 22:06:26 +0200

    Different icons for different achievements

--- src/app/types/achievement.type.ts
+++ src/app/types/achievement.type.ts
@@ -3,16 +3,19 @@ import { Observable, from } from 'rxjs';
 
 export interface Achievement {
   name: string;
+  icon: string;
   condition: (GameState) => boolean;
 }
 
 export const achievements$: Observable<Achievement> = from([
   {
-    name: 'First One At Work',
+    name: 'First Income',
+    icon: 'fa-euro-sign',
     condition: (state: GameState) => state.funds > 0,
   },
   {
     name: 'Educated',
+    icon: 'fa-university',
     condition: (state: GameState) => state.properties.education.value > 0,
   },
 ]);
