a17a253ab5e27271aac7326ca868b72b3d43532d
fboeller <fabianboeller@googlemail.com>
2020-05-15 19:44:30 +0200

    Add story for work button

---
+++ src/app/components/work-button/work-button.component.stories.ts
@@ -0,0 +1,24 @@
+import { moduleMetadata } from '@storybook/angular';
+import { initialState } from '../../types/game-state.type';
+import { provideMockStore } from '@ngrx/store/testing';
+import { WorkButtonComponent } from './work-button.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+export default {
+  title: 'Work Button',
+  decorators: [
+    moduleMetadata({
+      declarations: [WorkButtonComponent],
+      imports: [
+        BrowserAnimationsModule,
+      ],
+      providers: [
+        provideMockStore({ initialState: { gameState: initialState } }),
+      ],
+    }),
+  ],
+};
+
+export const withInitialState = () => ({
+  component: WorkButtonComponent,
+});
