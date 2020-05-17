566a4f5e835f31a9b3fcad55b6bde0b20b9ec29d
fboeller <fabianboeller@googlemail.com>
2020-05-17 11:55:21 +0200

    Remove increase type

--- src/app/components/property-row/property-row.component.spec.ts
+++ src/app/components/property-row/property-row.component.spec.ts
@@ -1,7 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { PropertyRowComponent } from './property-row.component';
-import { plus } from 'types/increase.type';
 
 describe('PropertyRowComponent', () => {
   let component: PropertyRowComponent;
