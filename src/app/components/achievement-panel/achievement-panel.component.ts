514c530c3ccb858222aacd7af21c612d208ee98f
fboeller <fabianboeller@googlemail.com>
2020-05-21 22:41:39 +0200

    Also list unachieved achievements

--- src/app/components/achievement-panel/achievement-panel.component.ts
+++ src/app/components/achievement-panel/achievement-panel.component.ts
@@ -1,15 +1,25 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnChanges } from '@angular/core';
 import { achievementMap } from 'types/achievement.type';
+import { keys, without, flow } from 'lodash/fp';
 
 @Component({
   selector: 'app-achievement-panel',
   templateUrl: './achievement-panel.component.html',
   styleUrls: ['./achievement-panel.component.styl'],
 })
-export class AchievementPanelComponent {
+export class AchievementPanelComponent implements OnChanges {
   @Input() achievements: string[];
 
   achievementMap = achievementMap;
 
+  unachieved: string[];
+
   constructor() {}
+
+  ngOnChanges() {
+    this.unachieved = flow(
+      keys,
+      without(this.achievements)
+    )(this.achievementMap);
+  }
 }
