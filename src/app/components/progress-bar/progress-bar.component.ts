0c5a3f8730fa3528e108e97010795c071cf5e6b3
fboeller <fabianboeller@googlemail.com>
2020-06-05 20:35:31 +0200

    Inferred animation duration

--- src/app/components/progress-bar/progress-bar.component.ts
+++ src/app/components/progress-bar/progress-bar.component.ts
@@ -1,14 +1,13 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-progress-bar',
   templateUrl: './progress-bar.component.html',
   styleUrls: ['./progress-bar.component.styl'],
 })
-export class ProgressBarComponent implements OnInit {
-  @Input() active: Boolean;
+export class ProgressBarComponent {
+  @Input() active: boolean;
+  @Input() duration: number;
 
   constructor() {}
-
-  ngOnInit(): void {}
 }
--- src/app/components/work-button/work-button.component.ts
+++ src/app/components/work-button/work-button.component.ts
@@ -1,24 +1,14 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
-import { Observable, Subject, interval, from } from 'rxjs';
+import { Observable, Subject, interval } from 'rxjs';
 import {
   withLatestFrom,
-  filter,
-  distinctUntilChanged,
   flatMap,
   take,
-  tap,
   endWith,
   map,
   share,
 } from 'rxjs/operators';
-import {
-  trigger,
-  state,
-  style,
-  transition,
-  animate,
-} from '@angular/animations';
-import { Store, select, Action } from '@ngrx/store';
+import { Store, select } from '@ngrx/store';
 import { AppState, work } from 'actions/game.actions';
 import { Selectors, selectGameState } from 'selectors/game.selectors';
 import { ofType, Actions } from '@ngrx/effects';
@@ -26,25 +16,6 @@ import { ofType, Actions } from '@ngrx/effects';
 @Component({
   selector: 'app-work-button',
   templateUrl: './work-button.component.html',
-  animations: [
-    trigger('startWork', [
-      state(
-        'workInProgress',
-        style({
-          width: '100%',
-        })
-      ),
-      state(
-        'noWorkInProgress',
-        style({
-          width: '0%',
-        })
-      ),
-      transition('noWorkInProgress => workInProgress', [
-        animate('{{ duration }}ms'),
-      ]),
-    ]),
-  ],
 })
 export class WorkButtonComponent implements OnInit {
   constructor(private store: Store<AppState>, private actions$: Actions) {}
@@ -60,18 +31,6 @@ export class WorkButtonComponent implements OnInit {
   ngOnInit() {
     this.animationDuration$ = this.store.pipe(select(Selectors.workDuration));
     this.workActive$ = this.store.pipe(select('gameState', 'workActive'));
-    this.progress$ = this.actions$.pipe(
-      ofType(work),
-      withLatestFrom(this.animationDuration$, (_, duration) => duration),
-      flatMap((duration) =>
-        interval(duration / 20).pipe(
-          take(20),
-          map((value) => value * 5),
-          endWith(100)
-        )
-      ),
-      share()
-    );
     const workEffect$ = this.store.pipe(
       select(selectGameState),
       select(Selectors.value, { property: 'salary' })
