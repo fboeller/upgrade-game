3de2bcfb40b470db3ec83e4dc13427c5352c80c4
fboeller <fabianboeller@googlemail.com>
2020-06-06 20:41:03 +0200

    Separate actions from reducers

--- src/app/game.actions.ts
+++ src/app/game.actions.ts
@@ -1,11 +1,8 @@
-import { createAction, createReducer, on, props } from '@ngrx/store';
+import { createAction, props } from '@ngrx/store';
 import { Property } from 'types/property.type';
-import { concat } from 'lodash/fp';
-import { GameState, initialState } from 'types/game-state.type';
+import { GameState } from 'types/game-state.type';
 import { Achievement } from 'types/achievement.type';
 import { Powerup } from 'types/powerup.type';
-import { Selectors } from 'selectors/game.selectors';
-import { PowerupSelectors } from 'selectors/powerup.selectors';
 
 export interface AppState {
   gameState: GameState;
@@ -38,52 +35,3 @@ export const deactivatePowerup = createAction(
   '[Powerup] Deactivated',
   props<{ powerup: Powerup }>()
 );
-
-const stateReducer0 = createReducer(
-  initialState,
-  on(resume, (state) => ({ ...state, timeActive: true })),
-  on(pause, (state) => ({ ...state, timeActive: false })),
-  on(income, (state, { property }) => ({
-    ...state,
-    funds: state.funds + Selectors.value(state, { property }),
-    workActive: property === 'salary' ? false : state.workActive,
-  })),
-  on(work, (state) => ({ ...state, workActive: true })),
-  on(upgrade, (state, { property }) => ({
-    ...state,
-    funds: state.funds - Selectors.upgradeCost(state, { property }),
-    properties: {
-      ...state.properties,
-      [property]: Selectors.level(state, { property }) + 1,
-    },
-  })),
-  on(propertyRevealed, (state, { property }) => ({
-    ...state,
-    properties: {
-      ...state.properties,
-      [property]: 0,
-    },
-  })),
-  on(achievementUnlocked, (state, { achievement }) => ({
-    ...state,
-    achievements: concat(achievement, state.achievements),
-  })),
-  on(activatePowerup, (state, { powerup }) => ({
-    ...state,
-    powerups: {
-      ...state.powerups,
-      [powerup]: PowerupSelectors.powerup(state, { powerup }) + 1,
-    },
-  })),
-  on(deactivatePowerup, (state, { powerup }) => ({
-    ...state,
-    powerups: {
-      ...state.powerups,
-      [powerup]: Math.max(0, PowerupSelectors.powerup(state, { powerup }) - 1),
-    },
-  }))
-);
-
-export function stateReducer(state, action) {
-  return stateReducer0(state, action);
-}
---
+++ src/app/game.reducer.ts
@@ -0,0 +1,65 @@
+import { createReducer, on } from '@ngrx/store';
+import { initialState } from 'types/game-state.type';
+import {
+  resume,
+  pause,
+  income,
+  work,
+  upgrade,
+  propertyRevealed,
+  achievementUnlocked,
+  activatePowerup,
+  deactivatePowerup,
+} from './game.actions';
+import { Selectors } from 'selectors/game.selectors';
+import { PowerupSelectors } from 'selectors/powerup.selectors';
+import { concat } from 'lodash/fp';
+
+const stateReducer0 = createReducer(
+  initialState,
+  on(resume, (state) => ({ ...state, timeActive: true })),
+  on(pause, (state) => ({ ...state, timeActive: false })),
+  on(income, (state, { property }) => ({
+    ...state,
+    funds: state.funds + Selectors.value(state, { property }),
+    workActive: property === 'salary' ? false : state.workActive,
+  })),
+  on(work, (state) => ({ ...state, workActive: true })),
+  on(upgrade, (state, { property }) => ({
+    ...state,
+    funds: state.funds - Selectors.upgradeCost(state, { property }),
+    properties: {
+      ...state.properties,
+      [property]: Selectors.level(state, { property }) + 1,
+    },
+  })),
+  on(propertyRevealed, (state, { property }) => ({
+    ...state,
+    properties: {
+      ...state.properties,
+      [property]: 0,
+    },
+  })),
+  on(achievementUnlocked, (state, { achievement }) => ({
+    ...state,
+    achievements: concat(achievement, state.achievements),
+  })),
+  on(activatePowerup, (state, { powerup }) => ({
+    ...state,
+    powerups: {
+      ...state.powerups,
+      [powerup]: PowerupSelectors.powerup(state, { powerup }) + 1,
+    },
+  })),
+  on(deactivatePowerup, (state, { powerup }) => ({
+    ...state,
+    powerups: {
+      ...state.powerups,
+      [powerup]: Math.max(0, PowerupSelectors.powerup(state, { powerup }) - 1),
+    },
+  }))
+);
+
+export function stateReducer(state, action) {
+  return stateReducer0(state, action);
+}
